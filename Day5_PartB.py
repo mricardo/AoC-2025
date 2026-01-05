4098d6fe594ba3ce518bcae84a8138808549180c
Miguel C. Ricardo <miguelcricardo@gmail.com>
2026-01-05 12:45:19 +0000

    Add files via upload

--- Day5_PartB.py
+++ Day5_PartB.py
@@ -1,10 +1,8 @@
-from sortedcontainers import SortedSet
-
 def solve():
     with open("Day5_Input.txt") as f:
         database = [s.strip('\n') for s in f.readlines()]
     
-    fresh_ingrediantes = SortedSet()
+    fresh_ingrediants = []
     
     for row in database:
         if len(row) == 0:
@@ -13,41 +11,22 @@ def solve():
         l = list(map(int, row.split("-")))
         start, end = l[0], l[1]
 
-        fresh_ingrediantes.add((start, end))
+        fresh_ingrediants.append((start, end))
     
-    current_walk = (0, 0)
+    fresh_ingrediants.sort()
+    curr_start, curr_end = fresh_ingrediants[0]
     new_walks = []
-    #O(n)
-    for fi in fresh_ingrediantes:
-        if current_walk == (0,0):
-            current_walk = fi
-            continue
-
-        # new interval
-        if fi[0] > current_walk[1]:
-            new_walks.append(current_walk)
-            current_walk = fi
-            continue
-        elif fi[0] == current_walk[1]:
-            current_walk = (current_walk[0], fi[1])
-            continue
-        elif fi[1] >= current_walk[1]:
-            if (current_walk[0] <= fi[0] < current_walk[1]) or \
-               (current_walk[0] < fi[0] <= current_walk[1]):
-                current_walk = (current_walk[0], fi[1])
-                continue
-            
-    new_walks.append(current_walk)
-
-    print(new_walks)
-    total_walks = 0
-    for nw in new_walks:
-        total_walks += (nw[1] - nw[0] + 1)
     
-    # 328809021493328 too low
-    # 347338785050534 wrong answer
-    # 347338785050518
-    # 347338785050515
+    for next_start, next_end in fresh_ingrediants[1:]:        
+        if next_start <= curr_end + 1:
+            curr_end = max(curr_end, next_end)
+        else:
+            new_walks.append((curr_start, curr_end))
+            curr_start, curr_end = next_start, next_end
+            
+    new_walks.append((curr_start, curr_end))
+    total_walks = sum(end - start + 1 for start, end in new_walks)
+
     return total_walks
 
 if __name__ == "__main__":
